5435c67a14f5b31a4a2dce0f4c8baf47c7d5ca22
lekhaLB <blekha2003@gmail.com>
2026-01-07 08:48:46 +0530

    3rd

---
+++ 3rdProg.cs
@@ -0,0 +1,15 @@
+/*
+Start with creating a terrain - 
+Go to windows-> package manager, click on unity registry and search for terrain tools, click on install button, then after installation click on ‘download asset from terrain store’ button on the same page, it will lead you to a page, and click on install there
+Similarly do for ‘starter assets first person updates’. 
+
+Now in the hierarchy window, create a 3d object - terrain. ( position -500,0,-500)
+Next go to paint terrain and select ‘raise or lower terrain’ from the drop down menu
+Now click on brush of your choice and raise the terrain - which makes the island
+
+Now we have to make water, so create 3d object and make new terrain ( position it 0,0.5,0) (scale 100,1,100). Now finish off the island as he did in the vid
+
+Now from the drop down, select smooth height and smoothen the island soft soft
+
+Next create a new material -> assets ->create->  materials
+*/
